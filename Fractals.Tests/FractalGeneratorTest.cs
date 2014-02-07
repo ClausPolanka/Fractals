f0363fe222646720f58f3f3242bebdab4c9ebdac
Claus Polanka <claus.polanka@chello.at>
2014-02-07 15:22:59 +0100

    Impl. for passing level 1 and therefore completing Fractal CatCoder.

--- Fractals.Tests/FractalGeneratorTest.cs
+++ Fractals.Tests/FractalGeneratorTest.cs
@@ -14,16 +14,33 @@ namespace Fractals.Tests
         [TestCase( 19683, 7,   442368)]
         [TestCase(531441, 7, 11943936)]
         [TestCase(531441, 9, 21233664)]
-        public void cat_coder_acceptance_tests(int triangleLength, int iterations, int perimeter)
+        public void cat_coder_acceptance_tests_level_1(int triangleLength, int iterations, int perimeter)
         {
-            var result = Perimeter(triangleLength, iterations);
+            var result = TriangleKochSnowflakePerimeter(triangleLength, iterations);
             Assert.AreEqual(perimeter, result, "perimeter");
         }
 
-        public int Perimeter(int length, int iterations)
+        [TestCase(   243, 3,      4500)]
+        [TestCase( 19683, 7,   2812500)]
+        [TestCase(531441, 7,  75937500)]
+        [TestCase(531441, 9, 210937500)]
+        public void cat_coder_acceptance_tests_level_2(int triangleLength, int iterations, int perimeter)
+        {
+            var result = SquareKochSnoflakePerimeter(triangleLength, iterations);
+            Assert.AreEqual(perimeter, result, "perimeter");
+        }
+
+        public int TriangleKochSnowflakePerimeter(int length, int iterations)
         {
             // Copied formula from http://en.wikipedia.org/wiki/Koch_snowflake
             return (int) Math.Round(3 * length * Math.Pow((double) 4 / 3, iterations));
+        }
+
+        private int SquareKochSnoflakePerimeter(int squareLength, int iterations)
+        {
+            var numberOfSidesAfterIteration = 4 * Math.Pow(5, iterations);
+            var sideLengthAfterIteration = squareLength / Math.Pow(3, iterations);
+            return (int) Math.Round(numberOfSidesAfterIteration * sideLengthAfterIteration);
         }
     }
 }
