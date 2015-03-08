70cdb79f2444882ab3d07035a26e96d80274a5ba
Claus Polanka <claus.polanka@chello.at>
2015-03-08 11:04:50 +0100

    [Red] level-1 first failing test for triangle from spec.

---
+++ Fractals.Tests/FractalTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fractals.Tests
+{
+    [TestFixture]
+    public class FractalTest
+    {
+        [TestCase(9, 1, 36)]
+        public void Perimeter_after_given_number_of_iterations(
+            int length, 
+            int iteration,
+            int expected)
+        {
+            var sut = new Fractal(length);
+            var perimeter = sut.Perimeter(iteration);
+            Assert.That(perimeter, Is.EqualTo(expected), 
+                string.Format("perimeter of triangle with l={0}, after {1} iteration(s).", 
+                length, 
+                iteration));
+        }
+    }
+}
---
+++ Fractals/Fractal.cs
@@ -0,0 +1,17 @@
+﻿namespace Fractals
+{
+    public class Fractal
+    {
+        private readonly int length;
+
+        public Fractal(int length)
+        {
+            this.length = length;
+        }
+
+        public int Perimeter(int interation)
+        {
+            return -1;
+        }
+    }
+}
